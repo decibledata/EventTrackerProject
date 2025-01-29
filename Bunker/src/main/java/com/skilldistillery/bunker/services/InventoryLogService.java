d657f540fd8bad7adbdafc3e45457f712765861c
George <villegas.george96@gmail.com>
2025-01-29 14:46:08 -0800

    Completed Working Inventory Logs Operations

---
+++ Bunker/src/main/java/com/skilldistillery/bunker/services/InventoryLogService.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.bunker.services;
+
+import java.util.List;
+
+import com.skilldistillery.bunker.entities.Inventory;
+import com.skilldistillery.bunker.entities.InventoryLog;
+
+public interface InventoryLogService {
+
+	List<InventoryLog> findAll();
+
+	InventoryLog findById(int id);
+
+	InventoryLog create(InventoryLog inventoryLogs);
+
+	boolean deleteById(int id);
+
+	InventoryLog update(int id, InventoryLog inventoryLogs);
+
+}
